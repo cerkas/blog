dc249bd4f9ebd4abc4a5e4b8083044f119be44c8
cerkas <cerkas1993@gmail.com>
2019-01-27 23:43:39 +0200

    Create save post ,from web application

--- api/main.go
+++ api/main.go
@@ -1,23 +1,25 @@
 package main
 
 import (
-  "encoding/json"
-  "fmt"
-  "github.com/go-martini/martini"
-  "io/ioutil"
-  "log"
-  "net/http"
-  "os"
-  "time"
+	"encoding/json"
+	"fmt"
+	"github.com/go-martini/martini"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+	"time"
 
-  "gopkg.in/mgo.v2"
+	"gopkg.in/mgo.v2"
 
-  "html/template"
+	"html/template"
 )
 
 type Post struct {
 	Text      string    `json:"text" bson:"text"`
+	Title     string    `json:"title" bson:"title"`
 	CreatedAt time.Time `json:"createdAt" bson:"created_at"`
+	Image     string `json:"image" bson:"image"`
 }
 
 var posts *mgo.Collection
@@ -36,22 +38,29 @@ func main() {
 	// Get posts collection
 	posts = session.DB("app").C("posts")
 
-	// Set up routes
-	/*r := mux.NewRouter()
+	/*// Set up routes
+	var dir string
+
+	flag.StringVar(&dir, "dir", ".", "web/assets")
+	flag.Parse()
+	fmt.Println(dir)
+	r := mux.NewRouter()
+	r.PathPrefix("./web/assets/").Handler(http.StripPrefix("./web/assets/", http.FileServer(http.Dir(dir))))
 	r.HandleFunc("/posts", createPost).
 		Methods("POST")
 	r.HandleFunc("/posts", readPosts).
+		Methods("GET")
+	r.HandleFunc("/", indexHandler).
 		Methods("GET")*/
   m := martini.Classic()
   //posts = make(map[string]*models.Post,0)
-  m.Post("/assets/",http.StripPrefix("/web/assets/",http.FileServer(http.Dir("./web/assets"))))
-  staticOptions := martini.StaticOptions{Prefix:"assets"}
-  m.Use(martini.Static("assets",staticOptions))
-
-  m.Get("/", indexHandler)
-  m.Post("/posts",createPost)
-	/*http.ListenAndServe(":8080", cors.AllowAll().Handler(r))
-	log.Println("Listening on port 8080...")*/
+	m.Post("/assets/",http.StripPrefix("/assets/",http.FileServer(http.Dir("./assets"))))
+	staticOptions := martini.StaticOptions{Prefix:"assets"}
+	m.Use(martini.Static("assets",staticOptions))
+	m.Get("/", indexHandler)
+	m.Get("/write" ,writeHandler)
+	m.Get("/news" ,newsHandler)
+	m.Post("/posts",createPost)
 	m.Run()
 }
 func indexHandler(w http.ResponseWriter, r *http.Request) {
@@ -61,7 +70,30 @@ func indexHandler(w http.ResponseWriter, r *http.Request) {
   }
   t.ExecuteTemplate(w,"index",nil)
 }
+func newsHandler(w http.ResponseWriter, r *http.Request) {
+	t, err := template.ParseFiles("web/templates/news.html","web/templates/header.html","web/templates/footer.html")
+	if err !=nil {
+		fmt.Printf( err.Error())
+	}
+	result := []Post{}
+	posts.Find(nil).Sort("-created_at").All(&result)
+	/*if err := posts.Find(nil).Sort("-created_at").All(&result); err != nil {
+		responseError(w, err.Error(), http.StatusInternalServerError)
+	} else {
+		responseJSON(w, result)
+	}*/
+	fmt.Print(result)
+	t.ExecuteTemplate(w,"news",result)
+}
+func writeHandler(w http.ResponseWriter, r *http.Request) {
+	t, err := template.ParseFiles("web/templates/write.html","web/templates/header.html","web/templates/footer.html")
+	if err !=nil {
+		fmt.Printf( err.Error())
+	}
+	t.ExecuteTemplate(w,"write",nil)
+}
 func createPost(w http.ResponseWriter, r *http.Request) {
+
 	// Read body
 	data, err := ioutil.ReadAll(r.Body)
 	if err != nil {
