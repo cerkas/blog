b917f3302ee8b6b6be2a7b2590f7b81c2e51793b
cerkas <cerkas1993@gmail.com>
2019-03-28 23:25:36 +0200

    Refactor

--- api/main.go
+++ api/main.go
@@ -19,7 +19,7 @@ type Post struct {
 	Text      string    `json:"text" bson:"text"`
 	Title     string    `json:"title" bson:"title"`
 	CreatedAt time.Time `json:"createdAt" bson:"created_at"`
-	Image     string `json:"image" bson:"image"`
+	Image     string    `json:"image" bson:"image"`
 }
 
 var posts *mgo.Collection
@@ -38,42 +38,28 @@ func main() {
 	// Get posts collection
 	posts = session.DB("app").C("posts")
 
-	/*// Set up routes
-	var dir string
-
-	flag.StringVar(&dir, "dir", ".", "web/assets")
-	flag.Parse()
-	fmt.Println(dir)
-	r := mux.NewRouter()
-	r.PathPrefix("./web/assets/").Handler(http.StripPrefix("./web/assets/", http.FileServer(http.Dir(dir))))
-	r.HandleFunc("/posts", createPost).
-		Methods("POST")
-	r.HandleFunc("/posts", readPosts).
-		Methods("GET")
-	r.HandleFunc("/", indexHandler).
-		Methods("GET")*/
-  m := martini.Classic()
-  //posts = make(map[string]*models.Post,0)
-	m.Post("/assets/",http.StripPrefix("/assets/",http.FileServer(http.Dir("./assets"))))
-	staticOptions := martini.StaticOptions{Prefix:"assets"}
-	m.Use(martini.Static("assets",staticOptions))
+	m := martini.Classic()
+	//posts = make(map[string]*models.Post,0)
+	m.Post("/assets/", http.StripPrefix("/assets/", http.FileServer(http.Dir("./assets"))))
+	staticOptions := martini.StaticOptions{Prefix: "assets"}
+	m.Use(martini.Static("assets", staticOptions))
 	m.Get("/", indexHandler)
-	m.Get("/write" ,writeHandler)
-	m.Get("/news" ,newsHandler)
-	m.Post("/posts",createPost)
+	m.Get("/write", writeHandler)
+	m.Get("/news", newsHandler)
+	m.Post("/posts", createPost)
 	m.Run()
 }
 func indexHandler(w http.ResponseWriter, r *http.Request) {
- t, err := template.ParseFiles("web/templates/index.html","web/templates/header.html","web/templates/footer.html")
-  if err !=nil {
-    fmt.Printf( err.Error())
-  }
-  t.ExecuteTemplate(w,"index",nil)
+	t, err := template.ParseFiles("web/templates/index.html", "web/templates/header.html", "web/templates/footer.html")
+	if err != nil {
+		fmt.Printf(err.Error())
+	}
+	t.ExecuteTemplate(w, "index", nil)
 }
 func newsHandler(w http.ResponseWriter, r *http.Request) {
-	t, err := template.ParseFiles("web/templates/news.html","web/templates/header.html","web/templates/footer.html")
-	if err !=nil {
-		fmt.Printf( err.Error())
+	t, err := template.ParseFiles("web/templates/news.html", "web/templates/header.html", "web/templates/footer.html")
+	if err != nil {
+		fmt.Printf(err.Error())
 	}
 	result := []Post{}
 	posts.Find(nil).Sort("-created_at").All(&result)
@@ -83,14 +69,14 @@ func newsHandler(w http.ResponseWriter, r *http.Request) {
 		responseJSON(w, result)
 	}*/
 	fmt.Print(result)
-	t.ExecuteTemplate(w,"news",result)
+	t.ExecuteTemplate(w, "news", result)
 }
 func writeHandler(w http.ResponseWriter, r *http.Request) {
-	t, err := template.ParseFiles("web/templates/write.html","web/templates/header.html","web/templates/footer.html")
-	if err !=nil {
-		fmt.Printf( err.Error())
+	t, err := template.ParseFiles("web/templates/write.html", "web/templates/header.html", "web/templates/footer.html")
+	if err != nil {
+		fmt.Printf(err.Error())
 	}
-	t.ExecuteTemplate(w,"write",nil)
+	t.ExecuteTemplate(w, "write", nil)
 }
 func createPost(w http.ResponseWriter, r *http.Request) {
 
